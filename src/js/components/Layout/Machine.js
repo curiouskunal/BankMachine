110d925a9eace691342649b06e5eb658e210e850
Number1Boss <N.Novak360@gmail.com>
2017-11-22 02:04:29 -0500

    Added buttonsg

--- src/js/components/Layout/Machine.js
+++ src/js/components/Layout/Machine.js
@@ -10,7 +10,7 @@ export default class Machine extends React.Component {
     return (
       <div id="content-wrapper">
         <Title title="Welcome to RBC" />
-        <JButton text="Francais"/>
+        <JButton text="Welcome"/>
         <JButton text="Francais"/>
       </div>
     );

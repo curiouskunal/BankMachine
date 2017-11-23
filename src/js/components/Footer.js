febfc4e90d01ae25fec1c7a153729cd9be62fe23
MatthewShortt <mattshortt4@gmail.com>
2017-11-23 02:09:45 -0500

    Home screen added

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer>Made by Nik Novak, Kunal Shah, Matt Short, Abdul</footer>
+      <footer>Made by Nik Novak, Kunal Shah, Matt Shortt, Abdul Ahad Akhter</footer>
     );
   }
 }
--- src/js/components/Layout/Machine/Screens/ScreenHome.js
+++ src/js/components/Layout/Machine/Screens/ScreenHome.js
@@ -15,19 +15,50 @@ export default class ScreenHome extends React.Component {
       <div id="screen-home">
 
         <div>
+         <div class = "left">
+            <JButton buttonclass="headerButton" text="BACK" icon="fa-arrow-circle-left" nav="../" {...this.props}/>
+          </div>
           <div class = "right">
             <JButton buttonclass="headerButton" text="EXIT" icon="fa-times-circle" nav="/" {...this.props}/>
           </div>
         </div>
         
         <Title title="Home" />
-        <div class="buttons-main">
-            <JButton text="Withdraw" img="/img/smile.png" nav="/signin/card" {...this.props}/>
-            <JButton text="Deposit" img="/img/smile.png" nav="/signin/mobile" {...this.props}/>
-            <JButton text="Transfer" img="/img/smile.png" nav="/test" {...this.props}/>
-            <JButton text="Check Account Balance" img="/img/smile.png" nav="/signin/card" {...this.props}/>
-            <JButton text="Manage Accounts" img="/img/smile.png" nav="/signin/mobile" {...this.props}/>
-            <JButton text="Sign Out" img="/img/smile.png" nav="/test" {...this.props}/>
+        
+         <div class="buttons-main">
+          <div class="col-md-4">
+             <div class='bouttons'>
+                <JButton buttonclass="boutton" text="Card sign in" icon="fa-credit-card-alt" nav="/signin/card" {...this.props}/>
+            </div>
+          </div>
+          <div class="col-md-4 ">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Mobile app sign in" icon="fa-mobile" nav="/signin/mobile" {...this.props}/>
+            </div>         
+           </div>
+          <div class="col-md-4">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Account number sign in" icon="fa-hashtag" nav="/signin/mobile" {...this.props}/>
+            </div>         
+            </div>
+            <div class="col-md-4">
+             <div class='bouttons'>
+                <JButton buttonclass="boutton" text="Card sign in" icon="fa-credit-card-alt" nav="/signin/card" {...this.props}/>
+            </div>
+          </div>
+          <div class="col-md-4 ">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Mobile app sign in" icon="fa-mobile" nav="/signin/mobile" {...this.props}/>
+            </div>         
+           </div>
+          <div class="col-md-4">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Account number sign in" icon="fa-hashtag" nav="/signin/mobile" {...this.props}/>
+            </div>         
+            </div>
+      
+    
+
         </div>
       </div>
     );

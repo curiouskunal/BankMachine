5a592ef76097af6bca21eecb0e4fc14eceab972e
Number1Boss <N.Novak360@gmail.com>
2017-11-22 21:52:11 -0500

    fixed bootstrap on buttons

--- src/js/components/Layout/Machine/Screens/ScreenSignIn.js
+++ src/js/components/Layout/Machine/Screens/ScreenSignIn.js
@@ -24,15 +24,15 @@ export default class ScreenSignIn extends React.Component {
         
         <Title title="Sign In" />
         <div class="buttonWrapper">
-          <div class="col-md-4">
-              <JButton buttonclass="HomeButton" text="I have my card" img="/img/smile.png" nav="/signin/card" {...this.props}/>
-          </div>
-          <div class="col-md-4">
-              <JButton buttonclass="HomeButton" text="Mobile app sign in" img="/img/smile.png" nav="/signin/mobile" {...this.props}/>
-          </div>
-          <div class="col-md-4">
-              <JButton buttonclass="HomeButton" text="Account number" img="/img/smile.png" nav="/test" {...this.props}/>
-          </div>
+          
+              <JButton bootstrap="col-md-4" buttonclass="HomeButton" text="I have my card" img="/img/smile.png" nav="/signin/card" {...this.props}/>
+          
+          
+              <JButton bootstrap="col-md-4" buttonclass="HomeButton" text="Mobile app sign in" img="/img/smile.png" nav="/signin/mobile" {...this.props}/>
+          
+          
+              <JButton bootstrap="col-md-4" buttonclass="HomeButton" text="Account number" img="/img/smile.png" nav="/test" {...this.props}/>
+          
         </div>
 
         

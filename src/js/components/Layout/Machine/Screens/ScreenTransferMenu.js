1d763a02c797870a927bc8370a029c452cff505a
MatthewShortt <mattshortt4@gmail.com>
2017-11-23 03:54:43 -0500

    Transfer Menu Added

--- src/js/components/Layout/Machine/Screens/ScreenTransferMenu.js
+++ src/js/components/Layout/Machine/Screens/ScreenTransferMenu.js
@@ -12,20 +12,43 @@ export default class TransferMenu extends React.Component {
   render() {
       console.log(this.props);
     return (
-      <div id="screen-trasnfermenu">
+      <div id="screen-transfermenu">
         <div >
 	        <div class = "left">
-				<JButton text="BACK" nav="../" {...this.props}/>
-			</div>
+            <JButton buttonclass="headerButton" text="BACK" icon="fa-arrow-circle-left" nav="../" {...this.props}/>
+          </div>
+          <div class = "right">
+            <JButton buttonclass="headerButton" text="HOME" icon="fa-home" nav="/" {...this.props}/>
+          </div>
 		</div>
         
         <Title title="Transfer Menu" />
         <div class="buttons-main">
-            <JButton text="Between your Accounts" img="/img/smile.png" nav="/signin/card" {...this.props}/>
-            <JButton text="e-Transfer" img="/img/smile.png" nav="/signin/mobile" {...this.props}/>
-            <JButton text="To another RBC Member" img="/img/smile.png" nav="/test" {...this.props}/>
-            <JButton text="Pay Bills" img="/img/smile.png" nav="/signin/card" {...this.props}/>
+          <div class="col-md-4 col-md-offset-2">
+             <div class='bouttons'>
+                 <JButton buttonclass="boutton" text="Between your Accounts" icon="fa-exchange" nav="/signin/card" {...this.props}/>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="e-Transfer" icon="fa-envelope-o" nav="/signin/mobile" {...this.props}/>
+            </div>         
+           </div>
+          <div class="col-md-4 col-md-offset-2">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="To another TD Member" icon="fa-user-o" nav="/test" {...this.props}/>
+            </div>         
+            </div>
+            <div class="col-md-4">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Pay Bills" icon="fa-money" nav="/signin/card" {...this.props}/>
+            </div>         
+            </div>
+      
+    
+
         </div>
+        
       </div>
     );
   }

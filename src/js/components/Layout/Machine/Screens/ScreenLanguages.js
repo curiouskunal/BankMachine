6b77be9b1be331f91d1a111901c4ffe8961df3f3
MatthewShortt <mattshortt4@gmail.com>
2017-11-24 08:31:14 -0500

    Finished other languages page

--- src/js/components/Layout/Machine/Screens/ScreenLanguages.js
+++ src/js/components/Layout/Machine/Screens/ScreenLanguages.js
@@ -16,69 +16,48 @@ export default class ScreenLanguages extends React.Component {
          <div class="tabletext">
             <h1>Other Languages</h1>
         </div>
-           <div class="table">
-               <table class="table-fill">
-                <tbody class="table-hover">
-                    <tr>
-                        <td>Dutch</td>
-                    </tr>
-                    <tr>
-                        <td>Chinese</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                    <tr>
-                        <td>Spanish</td>
-                    </tr>
-                </tbody>
-                </table>
+                    
+           <div class="buttons-main">
+          <div class="col-md-4">
+             <div class='bouttons'>
+                <JButton buttonclass="boutton" text="Deutsch" img="/img/flags/germany.svg" nav="/withdraw" {...this.props}/>
+            </div>
+          </div>
+          <div class="col-md-4 ">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Español" img="/img/flags/spain.svg" nav="/deposit" {...this.props}/>
+            </div>         
            </div>
+          <div class="col-md-4">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Italiano" img="/img/flags/italy.svg" nav="/transfermenu" {...this.props}/>
+            </div>         
+            </div>
+            <div class="col-md-4">
+             <div class='bouttons'>
+                <JButton buttonclass="boutton" text="Čeština" img="/img/flags/czech_republic.svg" nav="/balance" {...this.props}/>
+            </div>
+          </div>
+          <div class="col-md-4 ">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Portuguese" img="/img/flags/portugal.svg" nav="/signin/mobile" {...this.props}/>
+            </div>         
+           </div>
+          <div class="col-md-4">
+            <div class='bouttons'>
+                  <JButton buttonclass="boutton" text="Hindi" img="/img/flags/india.svg" nav="/logout" {...this.props}/>
+            </div>         
+            </div>
+      
+        </div>
+           
+           
+           
+           
+           
+           
+           
+ 
     </div>
     );
   }

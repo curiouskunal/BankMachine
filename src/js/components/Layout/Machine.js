4c6a41e3a8738a57a09422c595289720ce176e3f
Number1Boss <N.Novak360@gmail.com>
2017-11-24 04:28:56 -0500

    finished withdraw branch

--- src/js/components/Layout/Machine.js
+++ src/js/components/Layout/Machine.js
@@ -58,8 +58,7 @@ export default class Machine extends React.Component {
                 <Route path="/loading" exact render={props => <ScreenLoading redirects={["/home"]} {...props} />}/>
                 <Route path="/settings" exact render={props => <ScreenSettings {...props} />}/>
                 <Route path="/pin" exact render={props => <ScreenNumKey title="Enter your PIN" redirects={["/home"]} {...props} />}/>
-                <Route path="/amountselect/custom" exact render={props => <ScreenNumKey title="Enter an Amount to Withdraw" redirects={["/transaction/confirm?type=withdraw&amt="]}{...props} />}/>
-                <Route path="/withdraw/confirm" exact render={props => <ScreenConfirm text="Withdraw?" no="/home" yes="/home" {...props} />}/>
+                <Route path="/withdraw/confirm" exact render={props => <ScreenConfirm text="Withdraw?" no="/home" yes="/print" {...props} />}/>
                 <Route path="/pass" exact render={props => <ScreenTextInput {...props} />}/>
            </Switch>
         
--- src/js/components/Layout/Machine/Screens/ScreenConfirm/ConfirmText.js
+++ src/js/components/Layout/Machine/Screens/ScreenConfirm/ConfirmText.js
@@ -1,18 +1,22 @@
 import React from "react";
-
+const queryString = require('query-string');
 
 //var $ = require('jquery');
 
 export default class JButton extends React.Component {
+    constructor(){
+     super();
+        this.state = {amt:"0"};
+    }
 
   render() { 
-      console.log("yghkkjn " );
-      console.log(this.props.location.search);
+       this.state.amt = queryString.parse(this.props.location.search).amt;
+         console.log(this.state.amt);
     return (
       <div class="confirmtext">
           <h2>{this.props.text}</h2>
-              <h1>{this.props.amt}</h1>
-          <h2>{"From " + this.props.from + " to " + this.props.to} </h2>
+              <h1>{this.state.amt}</h1>
+          <h2 class={(this.props.to==null&&this.props.from==null ) ? 'invis' : null}>{"From " + this.props.from + " to " + this.props.to} </h2>
     </div>
     );
   }

f8a6f8f42d810e2b602624d369a9f2200033a56e
Number1Boss <N.Novak360@gmail.com>
2017-11-24 15:54:22 -0500

    downlaod link

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -4,7 +4,10 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer>Made by Nik Novak, Kunal Shah, Matt Shortt, Abdul Ahad Akhter</footer>
+        <div>
+            <footer>Made by Nik Novak, Kunal Shah, Matt Shortt, Abdul Ahad Akhter</footer>
+            <a id="download" href="https://www.dropbox.com/s/1t41l3bf60omp3x/BankMachine.zip?dl=1">DOWNLOAD THE SOURCE CODE</a>
+        </div>
     );
   }
 }

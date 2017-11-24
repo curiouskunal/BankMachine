8f6306fbfe80524383154bc96b41b07f563af94b
Number1Boss <N.Novak360@gmail.com>
2017-11-24 02:22:15 -0500

    done fuckign with keyboards

--- src/js/components/Layout/Machine/Screens/ScreenTest.js
+++ src/js/components/Layout/Machine/Screens/ScreenTest.js
@@ -1,102 +1,16 @@
 import React from "react";
-import Title from "../Title";
-import JButton from "../JButton";
 
 //var $ = require('jquery');
 
-export default class ScreenHome extends React.Component {
-//    componentWillMount(){
-//        this.props.history.push('/welcome?ijustgotpushed=true');
-//    }
+export default class Test extends React.Component {
+
 
   render() {
-      console.log(this.props);
+      console.log(this.props.title);
     return (
-      <div id="screen-test">
-
-        <div>
-         <div class = "left">
-            <JButton buttonclass="headerButton" text="BACK" icon="fa-arrow-circle-left" nav="../" {...this.props}/>
-          </div>
-          <div class = "right">
-            <JButton buttonclass="headerButton" text="EXIT" icon="fa-times-circle" nav="/" {...this.props}/>
-          </div>
-        </div>
+      <div >
         
-        <Title title="Home" />
         
-         <div class="buttons-main">
-          <div class="col-md-2 col-md-offset-3">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="1" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="2" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="3" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          
-          
-          
-          <div class="col-md-2 col-md-offset-3">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="4" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="5" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="6" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-         
-          
-          
-          <div class="col-md-2 col-md-offset-3">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="7" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="8" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="9" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          
-          
-          <div class="col-md-2 col-md-offset-3">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="BKSP" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="0" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-2">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="CLEAR" nav="/transfermenu/betweenacct/confirm" {...this.props}/>
-            </div>
-          </div>
-          
-      
-        </div>
       </div>
     );
   }

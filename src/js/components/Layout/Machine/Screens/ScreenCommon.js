5240b0cdfa81094ad1d4cc2e89c02387fc7245fa
MatthewShortt <mattshortt4@gmail.com>
2017-11-25 03:09:27 -0500

    Common screen complete, Home and transfer screens updated

--- src/js/components/Layout/Machine/Screens/ScreenCommon.js
+++ src/js/components/Layout/Machine/Screens/ScreenCommon.js
@@ -13,37 +13,47 @@ export default class ScreenCommon extends React.Component {
       console.log(this.props);
     return (
       <div id="screen-common">
-        <Title title="Home" />
+       <div >
+	        <div class = "left">
+            <JButton buttonclass="headerButton" text="BACK" icon="fa-arrow-circle-left" nav="../" {...this.props}/>
+          </div>
+          <div class = "right">
+            <JButton buttonclass="headerButton" text="HOME" icon="fa-home" nav="/home" {...this.props}/>
+          </div>
+		</div>
+       
+       
+        <Title title="My Common Transfers" />
         
          <div class="buttons-main">
-          <div class="col-md-4">
-             <div class='bouttons'>
-                <JButton buttonclass="boutton" text="Withdraw" icon="fa-money" nav="/withdraw" {...this.props}/>
-            </div>
-          </div>
-          <div class="col-md-4 ">
+            <div class="col-md-4 ">
             <div class='bouttons'>
-                  <JButton buttonclass="boutton" text="Deposit" icon="fa-money" nav="/deposit" {...this.props}/>
+                  <JButton buttonclass="boutton" text="Deposit $1099 into Savings Account" icon="fa-money" nav="/confirm" {...this.props}/>
             </div>         
            </div>
           <div class="col-md-4">
+             <div class='bouttons'>
+                <JButton buttonclass="boutton" text="e-Transfer $410 to Bob Weeks" icon="fa-paper-plane-o" nav="/confirm" {...this.props}/>
+            </div>
+          </div>
+          <div class="col-md-4">
             <div class='bouttons'>
-                  <JButton buttonclass="boutton" text="Transfer" icon="fa-paper-plane-o" nav="/transfermenu" {...this.props}/>
+                  <JButton buttonclass="boutton" text="Withdraw $40 from Checking Account" icon="fa-money" nav="/confirm" {...this.props}/>
             </div>         
             </div>
             <div class="col-md-4">
              <div class='bouttons'>
-                <JButton buttonclass="boutton" text="Check Account Balance" icon="fa-file-text-o" nav="/balance" {...this.props}/>
+                <JButton buttonclass="boutton" text="Transfer $500 from Checking to Savings Account" icon="fa-exchange" nav="/confirm" {...this.props}/>
             </div>
           </div>
           <div class="col-md-4 ">
             <div class='bouttons'>
-                  <JButton buttonclass="boutton" text="Manage Accounts" icon="fa-cog" nav="/signin/mobile" {...this.props}/>
+                  <JButton buttonclass="boutton" text="Check Savings Account for Blood Money from Nigerian Prince" icon="fa-blind" nav="/confirm" {...this.props}/>
             </div>         
            </div>
           <div class="col-md-4">
             <div class='bouttons'>
-                  <JButton buttonclass="boutton" text="Sign Out" icon="fa-sign-out" nav="/logout" {...this.props}/>
+                  <JButton buttonclass="boutton" text="Transfer $300 into RRSP Account from Savings Account" icon="fa-exchange" nav="/confirm" {...this.props}/>
             </div>         
             </div>
       

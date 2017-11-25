324ec31fd983e72f9015e89909b87ce772aadd01
Number1Boss <N.Novak360@gmail.com>
2017-11-25 04:57:26 -0500

    fixed icons

--- src/js/components/Layout/Machine/JButton.js
+++ src/js/components/Layout/Machine/JButton.js
@@ -66,9 +66,7 @@ export default class JButton extends React.Component {
        </div>
         <button class={this.props.buttonclass==null? "" : this.props.buttonclass} onClick={(e) => {this.handleClick();}}>
             <img class={this.props.img == null? "invis":""} src={this.props.img == null? "": this.props.img} alt=""/>
-            <div class="button-icon">
             <i class={this.props.icon==null? "invis" : ("fa "+ this.props.icon) } aria-hidden="true"></i>
-            </div>
             <span>{this.props.text == null? "default button": this.props.text}</span>
             <span class={this.props.sym == null? "invis":"sym"}> {this.props.sym == null? "": this.props.sym}</span>
             <span class={this.props.texttwo == null? "invis":"span2"}> {this.props.texttwo == null? "": this.props.texttwo}</span>

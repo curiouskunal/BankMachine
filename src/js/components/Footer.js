d90557ee5acfa093d15e0b70fc9e4a5ee0667855
Kunal Shah <kunal8@gmail.com>
2017-11-26 16:26:52 -0500

    footer download link centering

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -5,8 +5,11 @@ export default class Footer extends React.Component {
   render() {
     return (
         <div>
-            <footer>Made by Nik Novak, Kunal Shah, Matt Shortt, Abdul Ahad Akhter</footer>
+            <footer>Made by Nik Novak, Kunal Shah, Matt Shortt, Abdul Ahad Akhter 
+            {}
             <a id="download" href="https://www.dropbox.com/s/1t41l3bf60omp3x/BankMachine.zip?dl=1">DOWNLOAD THE SOURCE CODE</a>
+            </footer>
+           
         </div>
     );
   }

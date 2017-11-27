a3452a679f48e85260773bda542480bc0b7e0223
Number1Boss <N.Novak360@gmail.com>
2017-11-27 01:10:40 -0500

    added ScreenTake

---
+++ src/js/components/Layout/Machine/Screens/ScreenTake.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Title from "../Title";
+import JButton from "../JButton";
+
+//var $ = require('jquery');
+
+export default class ScreenTake extends React.Component {
+//    componentWillMount(){
+//        this.props.history.push('/welcome?ijustgotpushed=true');
+//    }
+
+  render() {
+      console.log(this.props);
+    return (
+      <div id="screen-take">
+        <div>
+          <div class = "left">
+            <JButton buttonclass="headerButton" text="BACK" icon="fa-arrow-circle-left" nav="../" {...this.props}/>
+          </div>
+          <div class = "right">
+            <JButton buttonclass="headerButton" text="HOME" icon="fa-home" nav="/home" {...this.props}/>
+          </div>
+        </div>
+        
+        <Title title="Insert Cash or Cheque" />
+        <div class="buttons-main">
+          <div class="col-md-5 center line">
+          <div class="padCashInsert">
+           <i id="arrowdown" class="fa fa-long-arrow-down"></i>s
+            <p> Insert Cash or Cheque Below </p>
+            </div>
+          </div>
+        <div class="col-md-3 col-md-offset-1">
+            <img src="/img/Image-3.GIF"/>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+}

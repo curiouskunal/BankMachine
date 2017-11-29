1adcc0b3bb9b33546d950cfbb9240b330f29e8fb
Nik-Novak <N.Novak360@gmail.com>
2017-11-29 13:29:41 -0500

    added mobile screen, need to fix single mic access only

--- src/js/components/Layout/Machine/Screens/ScreenMobile.js
+++ src/js/components/Layout/Machine/Screens/ScreenMobile.js
@@ -2,13 +2,26 @@ import React from "react";
 import Title from "../Title";
 import JButton from "../JButton";
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import VoiceModule from "./Modules/VoiceModule";
 
 //var $ = require('jquery');
 
 export default class ScreenMobile extends React.Component {
-//    componentWillMount(){
-//        this.props.history.push('/welcome?ijustgotpushed=true');
-//    }
+    constructor(){
+      super();
+         this.state = {
+             voice:{
+                 accessibility:
+                    [
+                        {keys: ['scan', 'qr', 'code'], selector: '.buttons-main img',  msg: 'You selected QR code sign in. Please open the TD app and scan the code.', help:"QR code. Allows you to sign in by scanning a QR code using your phone. Requires the TD secure app."},
+                        {keys: ['mobile', 'tap', 'nfc'], selector: '.buttons-main i',  msg: 'You selected Mobile tap sign in. Please ensure NFC is on and tap your phone against the screen.', help:"Mobile tap sign in. Allows you to sign in by tapping your phone to the screen. Requires the TD secure app."},
+                        {keys: ['back', 'undo'], selector: '.left button',  msg: 'You selected Back. Returning to previous menu.', help:"Back. Returns you to the previous menu."},
+                        {keys: ['quit', 'exit', 'logout', 'cancel'], selector: '.right button',  msg: 'You selected Exit. You have been logged out.', help:"Exit. Resets all information and returns to Welcome Screen."}
+                    ],         
+                 help: "You are on the mobile sign in screen. From here you may select a method to sign in. You may say ... options at any time to explain your options. If you say ... accessibility, you may navigate the menus with your voice."       
+             }
+         };
+     }
 
   render() { 
       console.log(this.props);
@@ -37,7 +50,10 @@ export default class ScreenMobile extends React.Component {
             <p class="tap">Tap phone here</p>
           </div>
         </div>
-
+    
+        <div class='module-voice'>
+            <VoiceModule options={this.state.voice.accessibility} help={this.state.voice.help} {...this.props}/>
+        </div>
     </div>
     );
   }

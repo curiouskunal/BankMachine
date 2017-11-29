3d9fffd65dc3283fd28179ce45941c71b4b75f75
Nik-Novak <N.Novak360@gmail.com>
2017-11-29 06:30:40 -0500

    blank

--- README.md
+++
@@ -1,6 +0,0 @@
-#4HC3 Assessment 2
-## Instructions to run
-1. Navigate to root folder
-2. *NPM install*
-3. *NPM run dev*
-4. Navigate to localhost:8080 on a web browser 
--- src/js/client.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
-
-import Layout from "./components/Layout";
-
-const app = document.getElementById('app');
-ReactDOM.render(////<Layout/>, app
-    <Layout/>, app);

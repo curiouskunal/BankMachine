9d88d92966ec3bdaa2def0bb7f0ece8eb47a0fc0
MatthewShortt <mattshortt4@gmail.com>
2017-11-29 04:38:53 -0500

    Added list of added features and remove-cand gif

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -8,6 +8,7 @@ export default class Footer extends React.Component {
             <footer>Made by Nik Novak, Kunal Shah, Matt Shortt, Abdul Ahad Akhter 
             {}
             <a id="download" href="https://www.dropbox.com/s/1t41l3bf60omp3x/BankMachine.zip?dl=1">DOWNLOAD THE SOURCE CODE</a>
+            <a id="downloadPDF" href="List of Added Features - Bank Machine.pdf" download><p></p><i id="pdf" class="fa fa-file-pdf-o"></i> Download List of Added Features</a>
             </footer>
            
         </div>
